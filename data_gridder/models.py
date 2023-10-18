cc6e7dd4f98fc9488d9b84721fe2c8f15c3f57da
Code bee <codebee345@outlook.com>
2023-10-18 18:28:09 +0100

    modified models

--- data_gridder/models.py
+++ data_gridder/models.py
@@ -21,7 +21,7 @@ class Poll(models.Model):
     poll_author = models.TextField(blank=False, default='appended_document')
     appended_document = models.FileField(upload_to='documents', default='sampledoc.docx')
     original_doc_name = models.TextField(blank=True, default='document_name')
-    #created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     fields = models.JSONField()
     
     def __str__(self):
